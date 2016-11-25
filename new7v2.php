581a60acf5e4512cfefbe1ed132a648a0d187523
DobrininDmitrii <glav.pochtamt@inbox.ru>
2016-11-25 16:32:38 +0700

    some funct

--- new7v2.php
+++ new7v2.php
@@ -1,17 +1,16 @@
-7.2<?php
-245444
-amend
-  вапро
-  ролб
-4
+<?php
 
-44
-/* 555
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function abc(){
+	echo 'xxx';
+	
+	return 999;
+	
+}
 
-длорп
-4445
-44
-44уке
+
+function qwer(){
+	echo 'xxxx';
+	
+	return 12345;
+	
+}
