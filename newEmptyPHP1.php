352dfafde8ac7c01eadf0e3e6d6fa4127baa1b7b
DobrininDmitrii <glav.pochtamt@inbox.ru>
2016-11-27 10:35:36 +0700

    hellow world

--- newEmptyPHP1.php
+++ newEmptyPHP1.php
@@ -1,27 +1,7 @@
 <?php
 
-echo 'origin';//изменено в гитхабе
-echo '4';
-echo '3';
-echo '4';
-//2коммит//
-echo '2коммит';
-
-echo 'git log';
-jhgf
-echo '321';
-if ( 1 || 2) {
-    $b = 5;
-}
-taйцуке
-
-zzz
-new branch testing
-qqq
-df//\\\\//\\..,,//??/\\||
-qqqqqqqqqqqqqggffqqggqqqqqqqqqqqq
-ffвааf
-вввв
-qв
-авва
-а
+function hellow(){
+	
+	echo 'hellow world';
+	
+}//func hellow
