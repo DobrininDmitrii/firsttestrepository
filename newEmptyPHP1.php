2cdf3a0f5af9f29eb1930d0e86368bfb35d52c0d
DobrininDmitrii <glav.pochtamt@inbox.ru>
2016-12-01 15:40:19 +0700

    просто коммит
    подробно: просто коммит

--- newEmptyPHP1.php
+++ newEmptyPHP1.php
@@ -6,6 +6,32 @@ function hellow(){
 	
 }//func hellow
 
+echo '';
+echo "";
+echo "";
+if (true) {
+	
+}
+ else {
+	
+}//else
 
-tonewbrmasterrerere
+function functionName($param) {
+	
+}//function
+
+
+if (true) {
+	
+}//if
+if (true) {
+	
+}//if
+while (true) {
+	foreach ($array as $value) {
+		
+	}//foreach
+
+		
+}//while
 
